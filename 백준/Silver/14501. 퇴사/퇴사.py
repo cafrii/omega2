2ecac793743efc00fb088004729a630fcac3dbf6
cafrii's github for work <cafriidst@gmail.com>
2025-03-10 23:25:28 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,35 +1,24 @@
 
-def solve_recursive(Ti:list, Pi:list):
+def solve(Ti:list, Pi:list):
+    # dynamic programming version
     N = len(Ti)
 
-    #   day  1   2   ...  N-1   N
-    #  index 0   1   ...  N-2  N-1
-    #
-    # 모든 단위는 0-base 로.
+    max_price = [ 0 ] * N
+    # max_price[i] 는 day i 부터 고려했을 때의 최대 이익
 
-    def get_max_profit(start:int):
-        # start 날 부터 고려하였을 때의 최대 이익
+    def get_max_price(start):
+        # easy access method
+        return max_price[start] if 0 <= start < N else 0
 
-        if start > N-1:
-            return 0
-        if start == N-1:
-            return Pi[start] if Ti[start] == 1 else 0
-        # start < N-1
+    for k in range(N-1, -1, -1):
+        # take
+        p1 = (Pi[k] if k + Ti[k] <= N else 0) + get_max_price(k + Ti[k])
+        # skip
+        p2 = get_max_price(k+1)
+        max_price[k] = max(p1, p2)
 
-        # case 1: take
-        #  if end day <= N-1
-        if start + (Ti[start]-1) <= N-1:
-            p1 = Pi[start] + get_max_profit(start + Ti[start])
-        else:
-            p1 = 0 # cannot take this job
+    return max_price[0]
 
-        # case 2: skip
-        p2 = get_max_profit(start + 1)
-
-        # print(f"({start}) take[{start}]={p1}, skip[{start}]={p2}, max={max(p1, p2)}")
-        return max(p1, p2)
-
-    return get_max_profit(0)
 
 
 N = int(input().strip())
@@ -39,5 +28,4 @@ Pi = [0] * N
 for i in range(N):
     Ti[i], Pi[i] = map(int, input().split())
 
-print(solve_recursive(Ti, Pi))
-
+print(solve(Ti, Pi))
