f21255c366cd55f312951edfb31e9ae09a1d8930
cafrii's github for work <cafriidst@gmail.com>
2025-03-12 11:29:20 +0900

    [Bronze II] Title: 피보나치 수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -0,0 +1,12 @@
+
+def solve(n:int):
+    # 문제 정의 상, a[i] 는 i번째 수열.
+    #  a[0] 은 0번째 수열.
+    #  구해야 하는 n번째 수열은 a[n]
+    a = [0, 1] + ([0] * (n-1))
+    for i in range(2,n+1):
+        a[i] = a[i-2] + a[i-1]
+    return a[n]
+
+n = int(input().strip())
+print(solve(n))
