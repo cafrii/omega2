11261b3aec932cb740c4007dd5669259732a3728
cafrii's github for work <cafriidst@gmail.com>
2025-08-03 17:10:41 +0900

    [Gold IV] Title: 행성 연결, Time: 1196 ms, Memory: 125076 KB -BaekjoonHub

--- 백준/Gold/16398. 행성 연결/행성 연결.py
+++ 백준/Gold/16398. 행성 연결/행성 연결.py
@@ -1,56 +1,59 @@
 
 import sys
+import heapq
 from heapq import heappush,heappop
 
 def get_input():
     input = sys.stdin.readline
     N = int(input().rstrip())
-    costs = []  # heapq of format (c,i,j)
+    mat = []
     for i in range(N):
         m = list(map(int, input().split()))
         #assert len(m) == N, f'wrong row[{i}] length'
-        for j in range(i+1,N): # we care only in upper trangle
-            heappush(costs, (m[j],i,j))
-    return N,costs
+        mat.append(m)
+    return mat
 
-
-def solve(N:int, costs:list[tuple[int,int,int]])->int:
+def solve_prim(mat:list[list[int]])->int:
     '''
-    sort by cost
-    start merge N single-node trees into one unified tree
-    use mst kruscal algorithm
-    return total cost
     '''
-    # `costs` is heap-sorted list (ascending order)
+    N = len(mat)
+    #assert len(mat[0]) == N, "wrong matrix dimension"
 
-    roots = list(range(N))  # planet: 0 ~ N-1
+    # node indexing: node-0 is first node.
+    visited = [0]*N
+    # visited[k] is true if node-k is already belonged to tree
+    num_nodes = 0
+    sum_costs = 0
+    INF = int(1e8) + 1
+    costs = [INF]*N
+    # costs: hq 의 효율적인 관리를 위해, 각 노드 별 네트워크로의 최소 연결 비용을 추적한다.
 
-    def find_root(a:int):
-        if a == roots[a]:
-            return a
-        roots[a] = ra = find_root(roots[a])
-        return ra
+    start = 0 # start node
+    hq = [ (0, start)]
+    costs[start] = 0
+    # 후보 노드 힙 큐. 리스트 요소: (cost, node) 튜플
+    # cost 는 네트워크에 이미 소속된 임의 노드와 이 node와의 연결 비용.
+    # 최초 시작 노드는 cost 0 부터 시작.
 
-    # we will not make cycle.
-    # so, only N-1 flows are needed.
-    num_flow = 0
-    total_cost = 0
-
-    # for c,a,b in costs:
-    while costs:
-        c,a,b = heappop(costs)
-        ra,rb = find_root(a),find_root(b)
-        if ra == rb: # they are alredy in same tree. skip!
-            continue
-        roots[rb] = roots[b] = ra # merge two tree!
-        total_cost += c
-        num_flow += 1
-        if num_flow >= N-1:
+    while hq:
+        cur_cost,cur_node = heappop(hq)
+        if visited[cur_node]: continue
+        visited[cur_node] = 1
+        # hq 에서 pop 될 때 비로소 해당 노드가 네트워크에 연결됨.
+        num_nodes += 1
+        sum_costs += cur_cost
+        if num_nodes >= N:
             break
-    return total_cost
+        # 후보 등록
+        for nxt_node,nxt_cost in enumerate(mat[cur_node]):
+            if visited[nxt_node]: continue
+            if costs[nxt_node] <= nxt_cost: continue
+            # 연결 후보 노드 nxt_node 와 연결 cost 를 힙큐에 추가.
+            # 네트워크의 어느 노드와 연결되는지는 관심 사항 아님.
+            heappush(hq, (nxt_cost, nxt_node))
+    return sum_costs
 
 
 if __name__ == '__main__':
-    inp = get_input()
-    r = solve(*inp)
-    print(r)
+    mat = get_input()
+    print(solve_prim(mat))
