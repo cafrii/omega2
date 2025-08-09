4df0d831b2687c7905385fce8920b91edbbabc84
cafrii's github for work <cafriidst@gmail.com>
2025-08-09 13:14:19 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
@@ -7,62 +7,28 @@ def get_input():
     T = int(input().rstrip())
     for _ in range(T):
         N = int(input().rstrip())
-        graph = [ [] for k in range(N+1) ]
+        parents = [0]*(N+1)  # parent[k] 가 0 이면 node k 는 root
         for _ in range(N-1):
             p,c = map(int, input().split()) # parent, child
-            graph[p].append(c)
+            parents[c] = p
         a,b = map(int, input().split())
-        yield N,graph,a,b
+        yield parents,a,b
     return
 
-def solve(N:int, graph:list[list[int]], A:int, B:int)->int:
+def solve_with_set(parents:list[int], a:int, b:int)->int:
     '''
-    graph 관계를 parent, level 형식의 트리로 표현.
-    그 다음 level 비교를 통한 lca 알고리즘 사용.
     '''
-    # finding root..
-    is_child = [0]*(N+1)
-    for n in range(1,N+1):
-        for c in graph[n]: is_child[c] = 1
-    root = next(i for i in range(1,N+1) if not is_child[i])
-    # assume there is only one root.
-
-    parents = list(range(N+1))
-    levels = [-1]*(N+1)
-
-    # 트리 노드의 level 결정. dfs 탐색. non-recursive.
-    parents[root] = root
-    levels[root] = 0
-    stack = [root]
-
-    while stack:
-        node = stack.pop()
-        lvl = levels[node]
-        for c in graph[node]:
-            if levels[c] >= 0: continue # cycle 방지용. 문제 조건에 의해 없어도 되긴 함.
-            parents[c] = node
-            levels[c] = lvl+1
-            stack.append(c)
-
-    # A 와 B 의 LCA (least common ancestor) 탐색
-    def find_lca(a:int, b:int)->int:
-        # let a be deeper node
-        if levels[a] < levels[b]:
-            a,b = b,a
-        # go up until they are same level
-        lvlb = levels[b]
-        while levels[a] != lvlb:
-            a = parents[a]
-        # go up until they meet
-        while a != b:
-            a,b = parents[a],parents[b]
-        return a
-
-    return find_lca(A, B)
-
+    a_branch = set() # a 의 브랜치 노드들
+    while a:
+        a_branch.add(a)
+        a = parents[a]
+    while b:
+        if b in a_branch: return b
+        b = parents[b]
+    return 0  # 이런 경우는 발생하지 않음.
 
 if __name__ == '__main__':
     it = get_input()
     for inp in it:
-        r = solve(*inp)
+        r = solve_with_set(*inp)
         print(r)
