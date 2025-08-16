db04476dfd2f976082361f2f49b28f031f7e3c01
cafrii's github for work <cafriidst@gmail.com>
2025-08-16 23:40:01 +0900

    [Gold IV] Title: 주난의 난(難), Time: 108 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.py
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.py
@@ -1,6 +1,6 @@
 
 import sys
-from heapq import heappush, heappop
+from collections import deque
 
 def get_input():
     input = sys.stdin.readline
@@ -9,14 +9,14 @@ def get_input():
     map1 = []
     for _ in range(N):
         map1.append(input().rstrip())
-        assert len(map1[-1]) == M, "wrong width"
+        # assert len(map1[-1]) == M, "wrong width"
     y1,x1 = y1-1,x1-1
-    assert map1[y1][x1] == '*', "wrong *"
+    # assert map1[y1][x1] == '*', "wrong *"
     y2,x2 = y2-1,x2-1
-    assert map1[y2][x2] == '#', "wrong #"
+    # assert map1[y2][x2] == '#', "wrong #"
     return map1,y1,x1
 
-def solve(map1:list[str], sy:int, sx:int)->int:
+def solve_01bfs(grid:list[str], sy:int, sx:int)->int:
     '''
     Args:
         map:
@@ -24,48 +24,34 @@ def solve(map1:list[str], sy:int, sx:int)->int:
     Returns:
         number of jumps to reach to target
     '''
-    N,M = len(map1),len(map1[0])
+    N,M = len(grid),len(grid[0])
 
-    # convert map to mutable 2d grid
-    map2grid = lambda s: 9 if s=='#' else 1 if s=='1' else 0
-    grid = [ [ map2grid(map1[y][x]) for x in range(M) ] for y in range(N) ]
+    que = deque()
+    jumpmap = [ [0]*M for _ in range(N) ]
 
-    deltas = [ (1,0),(-1,0),(0,1),(0,-1) ]
+    que.append((sy,sx))
+    jumpmap[sy][sx] = 1
 
-    def search(sy:int, sx:int)->int:
-        '''
-        Args:
-            (sy,sx): start location
-        Returns:
-            number of jumps required to reach target
-        '''
-        jumps = 1
-        visited = [ [0]*M for _ in range(N) ]
-        que = [] # heap que
+    deltas = [ (0,1),(0,-1),(1,0),(-1,0) ]
+    while que:
+        y,x = que.popleft()
+        jumps = jumpmap[y][x]
 
-        heappush(que, (jumps, sy,sx))
-        visited[sy][sx] = 1
-
-        while que:
-            jumps,y,x = heappop(que)
-            for dy,dx in deltas:
-                ny,nx = y+dy,x+dx
-                if not (0<=ny<N and 0<=nx<M): continue
-                if visited[ny][nx]: continue
-                if grid[ny][nx] == 9: # target
-                    return jumps
-                if grid[ny][nx] == 1:
-                    visited[ny][nx] = 1
-                    heappush(que, (jumps+1,ny,nx))
-                else:
-                    heappush(que, (jumps,ny,nx))
-                    visited[ny][nx] = 1
-        # did not meet target
-        return jumps
-
-    return search(sy, sx)
+        for dy,dx in deltas:
+            ny,nx = y+dy,x+dx
+            if not (0<=ny<N and 0<=nx<M): continue # out-of-bound
+            if jumpmap[ny][nx]: continue # already visited
+            if grid[ny][nx] == '#': return jumps # reached to target
+            if grid[ny][nx] == '1':
+                que.append((ny,nx))
+                jumpmap[ny][nx] = jumps+1
+            else:
+                que.appendleft((ny,nx))
+                jumpmap[ny][nx] = jumps
+    # did not meet target. it should not happen
+    return -1
 
 if __name__ == '__main__':
     inp = get_input()
-    r = solve(*inp)
+    r = solve_01bfs(*inp)
     print(r)
