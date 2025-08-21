c41af4e44d4077b2c86979e1dad3aba2245813e5
cafrii's github for work <cafriidst@gmail.com>
2025-08-21 22:47:52 +0900

    [Silver III] Title: 돌 게임 3, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.py
@@ -0,0 +1,48 @@
+import sys
+
+def solve_dp(N:int)->str:
+    '''
+    Arg:
+        N: number of stones
+    Rule
+        two player match, player S and C
+        each player takes either 1, 3, or 4 stone(s) at his turn
+        the player who takes last stone wins
+        player S goes first
+    Returns
+        winner name initial ('S' or 'C')
+    '''
+    sz = max(4, N)
+    dp = [' ']*(sz+1)
+    # dp[k]:
+    # k 개의 돌이 놓여 있을 때 상근부터 게임을 시작하여 최적 play 했을 경우의 최종 승자
+    # '?': 정할 수 없는 경우
+    # 'S': 상근 승 확정
+    # 'C': 창영 승 확정
+    #
+    dp[1] = 'S'  # S:1 개 가져가면 됨
+    dp[2] = 'C'  # S1, C1. 다른 선택지 없음.
+    dp[3] = 'S'  # S3
+    dp[4] = 'S'  # S4
+    if N <= 4:
+        return dp[N]
+    '''
+        dp[5]의 경우:
+        상근 먼저 시작하는데, 한번에 게임을 끝낼 수 없음. (5개 집을 수 없음)
+        1, 3, 4 개를 집으면 이후 상태는 dp[4], dp[2], dp[1] 의 상태처럼 되는데 turn이 바뀐다.
+        dp[4], dp[2], dp[1] 중에서 C가 이기는 dp[2]가 되도록 선택하면 dp[5]도 S의 승이다.
+        즉, dp[k] 에 대해서, dp[k-1], dp[k-3], dp[k-4] 중 C가 이기는 게 하나라도 있다면
+        그것을 선택하면 S의 승 확정.
+    '''
+    for k in range(5, N+1):
+        if 'C' in ( dp[k-1], dp[k-3], dp[k-4] ):
+            dp[k] = 'S'
+        else:
+            dp[k] = 'C'
+    return dp[N]
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    N = int(input().rstrip())
+    w = solve_dp(N)
+    print('SK' if w=='S' else 'CY')
