c9947d528d281787ce7f45497cbdc13dc062b96c
cafrii's github for work <cafriidst@gmail.com>
2025-08-23 22:38:46 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 180 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
@@ -1,15 +1,17 @@
 
 import sys
 
-def solve(s:str)->int:
+def solve_fast(s:str)->int:
     n = len(s)
-    ss = set() # substring set
-    for i in range(n):
-        for j in range(i+1,n+1):
-            ss.add(s[i:j])
-    return len(ss)
+    cnt = 0
+    for sn in range(1,n+1): # substr length
+        ss = set() # substring set
+        for k in range(n+1-sn):
+            ss.add(s[k:k+sn])
+        cnt += len(ss)
+    return cnt
 
 if __name__ == '__main__':
     input = sys.stdin.readline
     s = input().rstrip()
-    print(solve(s))
+    print(solve_fast(s))
