fd14db2289749e6d4760178a4fb67afca90d2f67
cafrii's github for work <cafriidst@gmail.com>
2025-08-25 22:28:12 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 556 ms, Memory: 52496 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -1,30 +1,51 @@
-import sys
 
-'''
-과목: 1~ 양수.
-과목의 수: 1 ≤ N ≤ 1000
-선수조건:  0 ≤ M ≤ 500_000
-'''
+import sys
+from collections import deque
+from itertools import islice
+from typing import Iterable
 
 def get_input():
     input = sys.stdin.readline
     N,M = map(int, input().split())
-    links = []
+    graph = [ [] for _ in range(N+1) ]
     for _ in range(M):
         a,b = map(int, input().split())
-        links.append((a,b))
-    return N,links
+        graph[a].append(b)
+    return N,graph
 
-def solve(N:int, links:list[tuple[int,int]])->list[int]:
+def solve_ts(N:int, graph:list[list[int]])->Iterable[int]:
     '''
+    topological sorting
+    Kahn 알고리즘
     '''
-    links.sort()
-    dp = [1] * N
-    for a,b in links:
-        dp[b-1] = max(dp[a-1]+1, dp[b-1])
-    return dp
+    # 진입 차수 계산
+    incoming = [0] * (N+1)
+    for a in range(1,N+1):
+        for b in graph[a]:
+            incoming[b] += 1
+
+    que = deque()
+    result = [1] * (N+1) # 기본 차수. 독립 과목일 경우 1학기차에 수강.
+
+    # 진입 차수가 0인 노드 탐색하여 큐에 추가
+    for i in range(1,N+1):
+        if incoming[i] == 0:
+            que.append(i)
+
+    while que:
+        a = que.popleft()
+        # a 에서 시작하는 모든 간선 정보 제거
+        for b in graph[a]:
+            incoming[b] -= 1
+            result[b] = max(result[b], result[a]+1)
+            if incoming[b] == 0:
+                que.append(b)
+
+    return islice(result, 1, None) # result[0]은 제외
+
 
 if __name__ == '__main__':
     inp = get_input()
-    lst = solve(*inp)
+    lst = solve_ts(*inp)
     print(' '.join(map(str, lst)))
+
