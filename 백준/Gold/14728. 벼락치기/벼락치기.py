9c71e5aaa2b351d4bd8696eb5ba0134918078e15
cafrii's github for work <cafriidst@gmail.com>
2025-08-28 23:13:44 +0900

    [Gold V] Title: 벼락치기, Time: 148 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.py
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -10,32 +10,24 @@ def get_input():
         A.append((k, s)) # required study time, score
     return N,T,A
 
-def solve_dp(N:int, T:int, A:list[tuple[int,int]])->int:
+def solve_opt(N:int, T:int, A:list[tuple[int,int]])->int:
     '''
-        N: 시험 단원 수
         T: 주어진 총 공부 시간 (budget)
         A: 과목 정보 [ (reqt, score), .. ]
             reqt: required study time
             score:
     '''
-    dp = [0] * (T+1)
-    # dp[t] 는 공부시간 t가 주어졌을 때의 얻을 수 있는 최고 점수
-
-    for reqt,score in A:
-        # 각 과목 별로 하나씩 검토.
+    dp = [0] * (T+1) # dp[t] 는 공부시간 t가 주어졌을 때의 얻을 수 있는 최고 점수
+    for reqt,score in A: # 각 과목 별로 하나씩 검토.
         if reqt > T: continue
-
-        dp2 = dp[:] # clone
-
-        for t in range(reqt, T+1):
-            dp2[t] = max(
+        for t in range(T, reqt-1, -1):
+            dp[t] = max(
                 dp[t], # 이 과목을 공부하지 않는 경우. 그냥 이전 점수 유지.
                 dp[t-reqt] + score, # 이 과목을 선택
             )
-        dp = dp2
     return dp[T]
 
 if __name__ == '__main__':
     inp = get_input()
-    r = solve_dp(*inp)
-    print(r)
+    print(solve_opt(*inp))
+
