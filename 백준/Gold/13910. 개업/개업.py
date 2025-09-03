e93bc9f595a62d6ed5ac0046e994de75c41b6d36
cafrii's github for work <cafriidst@gmail.com>
2025-09-03 17:49:05 +0900

    [Gold V] Title: 개업, Time: 928 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/13910. 개업/개업.py
+++ 백준/Gold/13910. 개업/개업.py
@@ -1,5 +1,7 @@
 
+
 import sys
+from collections import deque
 
 def get_input():
     input = sys.stdin.readline
@@ -8,7 +10,7 @@ def get_input():
     assert len(W) == M, "wrong wok num"
     return N,W
 
-def solve(N:int, W:list[int])->int:
+def solve_bfs(N:int, W:list[int])->int:
     '''
     Args
         N: target num to compose. <= 10_000
@@ -19,21 +21,37 @@ def solve(N:int, W:list[int])->int:
     # 웍 두개 조합 사용. 총합 만 중요.
     for j in range(1, len(W)):
         for i in range(j):
-            ws.add(W[i]+W[j])
+            w2 = W[i] + W[j]
+            if w2 <= N: ws.add(w2)
+
+    ws = sorted(list(ws), reverse=True)
 
     INF = max(10_001, N+1)
-    dp = [ INF ] * (N+1)
-    # dp[k]: k 인분 식사를 준비하기 위해 필요한 최소 요리 회수
-    dp[0] = 0
+    dist = [ INF ] * (N+1)
+    dist[0] = 0
 
-    for k in range(1, N+1):
-        # dp[1] 부터 dp[N] 까지 단계적으로 계산
-        for w in ws: # 모든 wok 조합에 대해서
-            if w <= k:
-                dp[k] = min(dp[k], dp[k-w]+1)
+    que = deque()
+    que.extend(ws)
+    for w in ws: dist[w] = 1
 
-    return dp[N] if dp[N] < INF else -1
+    if dist[N] < INF: return 1
+
+    while que:
+        cur = que.popleft()
+        d = dist[cur]
+
+        for w in ws:
+            nxt = cur + w
+            if nxt == N: dist[N] = d+1; break
+            if nxt > N: continue
+            if dist[nxt] < INF: continue # already visited
+            que.append(nxt)
+            dist[nxt] = d + 1
+
+        if dist[N] < INF: break
+
+    return dist[N] if dist[N]<INF else -1
 
 
 if __name__ == '__main__':
-    print(solve(*get_input()))
+    print(solve_bfs(*get_input()))
