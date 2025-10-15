cd62e284a245aa0d1d5b8ae331a681c83efee0cf
cafrii's github for work <cafriidst@gmail.com>
2025-10-15 20:41:53 +0900

    [Gold IV] Title: 목장 건설하기, Time: 336 ms, Memory: 47048 KB -BaekjoonHub

--- 백준/Gold/14925. 목장 건설하기/목장 건설하기.py
+++ 백준/Gold/14925. 목장 건설하기/목장 건설하기.py
@@ -4,11 +4,11 @@ def get_input():
     M,N = map(int, input().split())
     A = []
     for _ in range(M):
-        A.append(list(map(int, input().split())))
+        A.append(list(input().split()))
         #assert len(A[-1]) == N
     return M,N,A
 
-def solve(M:int, N:int, A:list[list[int]])->int:
+def solve(M:int, N:int, A:list[list[str]])->int:
     '''
     Args: A: ground map, MxN matrix
             M: 세로길이, num of rows
@@ -18,7 +18,7 @@ def solve(M:int, N:int, A:list[list[int]])->int:
     max_l = 0
     for y in range(1, M+1):
         for x in range(1, N+1):
-            if A[y-1][x-1]: continue # 나무 또는 돌
+            if A[y-1][x-1] != '0': continue # 나무 또는 돌
             dp[y][x] = min(dp[y][x-1], dp[y-1][x], dp[y-1][x-1]) + 1
         max_l = max(max_l, max(dp[y]))
     return max_l
